9ab32d3dc8a306a94241e8860218b7358e5513ee
saupin <guillaume.saupin@gmail.com>
2025-01-26 17:01:44 +0000

    UX

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,29 +63,30 @@ else:
             documents,
         )
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("What is up?"):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        query_engine = index.as_query_engine()
+    query_engine = index.as_query_engine()
+    with st.spinner('Generating answer augmented by documents'):  
         res = query_engine.query(st.session_state.messages[-1]["content"])
 
 
-        with st.chat_message("assistant"):
-            response = st.write(res.response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.chat_message("assistant"):
+        response = st.write(res.response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
